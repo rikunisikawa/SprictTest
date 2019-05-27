2903b859085a21887e7fd0071f380f66cf721d1e
riku.nishikawa <basketball.back.0626@gmail.com>
2019-05-27 20:41:20 +0900

    Lesson4 課題

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,30 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss {
-	private int mp = 53;
-	public void Magic (){
-		this.mp =- 3;
-		if (mp > 0) {
-			Debug.Log ("魔法攻撃をした。残りMPは" + mp);
-		} else {
-			Debug.Log("「MPが足りないため魔法が使えない。」");
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int [] Array = { 1,2,3,4,5};
+	 for (int i = 0; i<5 ; i++)
+		{
+		 Debug.Log(Array[i]);
+		}
+		  for (int i = 4; i>=0 ; i--)
+		{
+		Debug.Log(Array[i]);
 		}
 	}
-	public class Test : MonoBehaviour {
-
-		// Use this for initialization
-		void Start() {
-			Boss boss = new Boss ();
-			boss.Magic ();
-			for (int i = 0; i < 4; i++)
-			{
-				Debug.Log (boss);
-			}
-		}
-
-		// Update is called once per frame
-		void Update(){
-		}
+	// Update is called once per frame
+	void Update () {
 	}
 }
